71fba909b796c059e2abd85ad6b549fc1ddcde4c
raashidbaba <raashidbaba849@gmail.com>
2023-02-28 23:47:31 +0530

    Added SleepQualityFragment.kt

--- app/src/main/java/com/example/sleeptracker/SleepQuality/SleepQualityFragment.kt
+++ app/src/main/java/com/example/sleeptracker/SleepQuality/SleepQualityFragment.kt
@@ -1,4 +1,22 @@
 package com.example.sleeptracker.SleepQuality
 
-class SleepQualityFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.sleeptracker.R
+import com.example.sleeptracker.databinding.FragmentSleepQualityBinding
+
+class SleepQualityFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentSleepQualityBinding>(inflater, R.layout.fragment_sleep_quality,container,false)
+        val application = requireNotNull(this.activity).application
+        return binding.root
+    }
 }

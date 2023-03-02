01e2b752caac87ed86c3b6d5aa1eabed53b001b2
raashidbaba <raashidbaba849@gmail.com>
2023-03-02 23:42:11 +0530

    Added SleepNight.kt

--- app/src/main/java/com/example/sleeptracker/SleepTracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/example/sleeptracker/SleepTracker/SleepTrackerViewModel.kt
@@ -1,4 +1,20 @@
 package com.example.sleeptracker.SleepTracker
 
-class SleepTrackerViewModel {
+import android.app.Application
+import android.arch.lifecycle.Transformations
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.example.sleeptracker.Database.SleepDatabaseDao
+import com.example.sleeptracker.Database.SleepNight
+
+class SleepTrackerViewModel(
+    val database: SleepDatabaseDao,
+    application: Application,
+) : AndroidViewModel(application) {
+   /* private var tonight = MutableLiveData<SleepNight?>()
+    private val night = database.getAllNights()
+
+    val nightsString = Transformations.map(night){
+        nights -> formatNights(nights,application.resources)*/
+    //}
 }
--- app/src/main/java/com/example/sleeptracker/SleepTracker/SleepTrackerViewModelFactory.kt
+++ app/src/main/java/com/example/sleeptracker/SleepTracker/SleepTrackerViewModelFactory.kt
@@ -1,4 +1,20 @@
 package com.example.sleeptracker.SleepTracker
 
-class SleepTrackerViewModelFactory {
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.sleeptracker.Database.SleepDatabaseDao
+
+class SleepTrackerViewModelFactory(
+    private val dataSource: SleepDatabaseDao,
+    private val application: Application
+
+) :ViewModelProvider.Factory{
+    @Suppress("unchecked_cast")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(SleepTrackerViewModel::class.java)) {
+            return SleepTrackerViewModel(dataSource, application) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }

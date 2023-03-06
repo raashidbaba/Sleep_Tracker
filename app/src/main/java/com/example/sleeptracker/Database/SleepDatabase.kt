a13154ab12f947096bb5f3ddd6db8b75b65b6aaa
raashidbaba <raashidbaba849@gmail.com>
2023-03-06 23:21:17 +0530

    added SleepDatabase.kt

--- app/src/main/java/com/example/sleeptracker/Database/SleepDatabase.kt
+++ app/src/main/java/com/example/sleeptracker/Database/SleepDatabase.kt
@@ -1,4 +1,36 @@
 package com.example.sleeptracker.Database
 
-class SleepDatabase {
+import android.content.Context
+import androidx.lifecycle.ViewModelProvider
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import kotlinx.coroutines.internal.synchronized
+
+
+@Database(entities = [SleepNight::class], version = 1, exportSchema = false)
+
+abstract class SleepDatabase : RoomDatabase() {
+    abstract val sleepDatabaseDao: SleepDatabaseDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: SleepDatabase? = null
+        fun getInstance(context: Context): SleepDatabase {
+            kotlin.synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        SleepDatabase::class.java,
+                        "sleep_history_database"
+                    )
+                        .fallbackToDestructiveMigration()
+                        .build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
 }

4d0c6a2552c4ae5edc3135c9493c6f067bfb508d
raashidbaba <raashidbaba849@gmail.com>
2023-03-26 17:30:09 +0530

    Add recyclerview

--- app/src/main/java/com/example/sleeptracker/sleepTracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/sleeptracker/sleepTracker/SleepNightAdapter.kt
@@ -1,5 +1,27 @@
 package com.example.sleeptracker.sleepTracker
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.sleeptracker.Database.SleepNight
+import com.example.sleeptracker.R
+import com.example.sleeptracker.TextItemViewHolder
 
-class SleepNightAdapter ()
+class SleepNightAdapter() : RecyclerView.Adapter<TextItemViewHolder>() {
+    var data = listOf<SleepNight>()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
+val layoutInflater = LayoutInflater.from(parent.context)
+        val view = layoutInflater.inflate(R.layout.text_item_view,parent,false) as TextView
+        return TextItemViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
+    val item = data[position]
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+}
